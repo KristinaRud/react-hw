18808399a75ee2abf8d3a5eb34a48d2f364b0c24
@kristina.rud5 <kristina.rud5@gmail.com>
2023-04-16 22:20:09 +0300

    changed useEffect

--- hw4/src/App.jsx
+++ hw4/src/App.jsx
@@ -1,29 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import Comics from "./components/Comics/Comics";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import "./styles/styles.scss";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import OrderBasket from "./pages/OrderBasket/OrderBasket";
 import NotPage from "./pages/NotPage/NotPage";
 import FavoriteItems from "./pages/FavoriteItems/FavoriteItems";
 import { useDispatch, useSelector } from "react-redux";
-import { actionSetFavorite, actionSetOrder } from "./reducer";
+import { actionSetFavorite, actionSetOrder,actionFetchSliderComics } from "./reducer";
 
 const App = () => {
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   if (localStorage.getItem(`arrFavorite`)) {
-  //     dispatch(
-  //       actionSetFavorite(JSON.parse(localStorage.getItem(`arrFavorite`)))
-  //     );
-  //   }
+  useEffect(() => {
+    dispatch(actionFetchSliderComics());
+    
+    if (localStorage.getItem(`arrFavorite`)) {
+      dispatch(
+        actionSetFavorite(JSON.parse(localStorage.getItem(`arrFavorite`)))
+      );
+    }
 
-  //   if (localStorage.getItem(`arrOrder`)) {
-  //     dispatch(actionSetOrder(JSON.parse(localStorage.getItem(`arrOrder`))));
-  //   }
-  // }, []);
+    if (localStorage.getItem(`arrOrder`)) {
+      dispatch(actionSetOrder(JSON.parse(localStorage.getItem(`arrOrder`))));
+    }
+  }, []);
 
   return (
     <div className="page__wrapper">
--- hw4/src/components/Comics/Comics.jsx
+++ hw4/src/components/Comics/Comics.jsx
@@ -1,26 +1,10 @@
 import "./Comics.scss";
-import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { actionFetchSliderComics,actionSetFavorite, actionSetOrder  } from "../../reducer";
 import Slider from "../Slider/Slider";
 
 const Comics = () => {
-  const dispatch = useDispatch();
   const dataComics = useSelector((state) => state.comics.slider);
   const loading = useSelector((state) => state.comics.isLoading);
-
-  useEffect(() => {
-    dispatch(actionFetchSliderComics());
-    if (localStorage.getItem(`arrFavorite`)) {
-      dispatch(
-        actionSetFavorite(JSON.parse(localStorage.getItem(`arrFavorite`)))
-      );
-    }
-
-    if (localStorage.getItem(`arrOrder`)) {
-      dispatch(actionSetOrder(JSON.parse(localStorage.getItem(`arrOrder`))));
-    }
-  }, []);
 
   return (
     <>
