691e73740a4b9a32cf675471e2b456d754427022
@kristina.rud5 <kristina.rud5@gmail.com>
2023-04-22 13:17:22 +0300

    changed slice comics

--- hw5/src/reducer/comics.slice.js
+++ hw5/src/reducer/comics.slice.js
@@ -22,7 +22,7 @@ const comicsSlice = createSlice({
 			if(localStorage.getItem(`arrFavorite`)){
 				const favor = (JSON.parse(localStorage.getItem(`arrFavorite`)))?.map((el) => {
 					const index = payload.findIndex((item) => item.id === el.id);
-					if (index !== 1) {
+					if (index !== -1) {
 					  return payload[index];
 					}
 				});
@@ -32,7 +32,7 @@ const comicsSlice = createSlice({
 			if(localStorage.getItem(`arrOrder`)){
 				const order = (JSON.parse(localStorage.getItem(`arrOrder`)))?.map((el) => {
 					const index = payload.findIndex((item) => item.id === el.id);
-					if (index !== 1) {
+					if (index !== -1) {
 					  return { ...payload[index], count: el.count };
 					}
 				});
